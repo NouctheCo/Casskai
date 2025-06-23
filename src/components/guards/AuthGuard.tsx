01fbb51bf93c8cf9a6592b5e8d9b539c1691d37c
NouctheCo <aldric.afannou@noutcheconseil.com>
2025-06-23 16:12:08 +0200

    Update AuthGuard.tsx

--- src/components/guards/AuthGuard.tsx
+++ src/components/guards/AuthGuard.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useSupabase } from '../hooks';
+import { useSupabase } from '../../hooks';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
