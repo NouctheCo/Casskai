c3a4030cde6a18f0647ef942c113f097f2d240dd
NouctheCo <aldric.afannou@noutcheconseil.com>
2025-12-27 02:25:21 +0100

    fix: HelpCenterPage syntax error - complete dark:bg class

--- src/pages/HelpCenterPage.tsx
+++ src/pages/HelpCenterPage.tsx
@@ -954,7 +954,7 @@ const HelpCenterPage = () => {
 
       {/* Section ressources supplémentaires */}
 
-      <div className="py-16 bg-white dark:bg-gray-
+      <div className="py-16 bg-white dark:bg-gray-900">
 
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
 
